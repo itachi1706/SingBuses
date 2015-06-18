3741e15d3fb69b5a9b858e57e66570a7a423fb27
Kenneth Soh <kennethsohyq@hotmail.com>
2015-06-18 21:14:14 +0800

    Fixed Resources (again)

--- src/bus_layout.c
+++ src/bus_layout.c
@@ -3,13 +3,13 @@
 
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
-static GBitmap *s_res_action_previous_icon;
-static GBitmap *s_res_action_refresh_icon;
-static GBitmap *s_res_action_next_icon;
+static GBitmap *s_res_actionicon_previous_white;
+static GBitmap *s_res_actionicon_next_white;
 static GFont s_res_gothic_24_bold;
 static GFont s_res_gothic_14;
 static GFont s_res_gothic_28_bold;
 static GFont s_res_gothic_18_bold;
+static GBitmap *s_res_actionicon_refresh_white;
 static ActionBarLayer *actionbar_layer;
 static TextLayer *textlayer_bus_no;
 static TextLayer *textlayer_busstop_name;
@@ -27,20 +27,20 @@ static void initialise_ui(void) {
     window_set_fullscreen(s_window, false);
   #endif
   
-  s_res_action_previous_icon = gbitmap_create_with_resource(RESOURCE_ID_ACTION_PREVIOUS_ICON);
-  s_res_action_refresh_icon = gbitmap_create_with_resource(RESOURCE_ID_ACTION_REFRESH_ICON);
-  s_res_action_next_icon = gbitmap_create_with_resource(RESOURCE_ID_ACTION_NEXT_ICON);
+  s_res_actionicon_previous_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_PREVIOUS_WHITE);
+  s_res_actionicon_next_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_NEXT_WHITE);
   s_res_gothic_24_bold = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
   s_res_gothic_14 = fonts_get_system_font(FONT_KEY_GOTHIC_14);
   s_res_gothic_28_bold = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
   s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+  s_res_actionicon_refresh_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_REFRESH_WHITE);
   // actionbar_layer
   actionbar_layer = action_bar_layer_create();
   action_bar_layer_add_to_window(actionbar_layer, s_window);
   action_bar_layer_set_background_color(actionbar_layer, GColorBlack);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_action_previous_icon);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_SELECT, s_res_action_refresh_icon);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_action_next_icon);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_actionicon_previous_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_SELECT, s_res_actionicon_refresh_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_actionicon_next_white);
   layer_add_child(window_get_root_layer(s_window), (Layer *)actionbar_layer);
   
   // textlayer_bus_no
@@ -96,7 +96,7 @@ static void initialise_ui(void) {
   
   // textlayer_debug
   textlayer_debug = text_layer_create(GRect(3, 136, 117, 20));
-  text_layer_set_text(textlayer_debug, "Text layer");
+  text_layer_set_text(textlayer_debug, "Debug Button");
   text_layer_set_font(textlayer_debug, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)textlayer_debug);
 }
@@ -113,9 +113,9 @@ static void destroy_ui(void) {
   bitmap_layer_destroy(bitmaplayer_current);
   bitmap_layer_destroy(bitmaplayer_next);
   text_layer_destroy(textlayer_debug);
-  gbitmap_destroy(s_res_action_previous_icon);
-  gbitmap_destroy(s_res_action_refresh_icon);
-  gbitmap_destroy(s_res_action_next_icon);
+  gbitmap_destroy(s_res_actionicon_previous_white);
+  gbitmap_destroy(s_res_actionicon_next_white);
+  gbitmap_destroy(s_res_actionicon_refresh_white);
 }
 // END AUTO-GENERATED UI CODE
 
