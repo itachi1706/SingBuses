74e430dada4671718b26928be52e9cef3cb4360b
Kenneth Soh <kennethsohyq@hotmail.com>
2015-06-18 21:49:00 +0800

    Finally fixed resources. GUI Roughly done
    TODO:
    The resource files for Seats Available, Limited Seating and Packed buses

--- src/bus_layout.c
+++ src/bus_layout.c
@@ -3,14 +3,13 @@
 
 // BEGIN AUTO-GENERATED UI CODE; DO NOT MODIFY
 static Window *s_window;
-static GBitmap *s_res_actionicon_previous_white;
-static GBitmap *s_res_actionicon_next_white;
 static GFont s_res_gothic_24_bold;
 static GFont s_res_gothic_14;
 static GFont s_res_gothic_28_bold;
 static GFont s_res_gothic_18_bold;
 static GBitmap *s_res_actionicon_refresh_white;
-static ActionBarLayer *actionbar_layer;
+static GBitmap *s_res_actionicon_previous_white;
+static GBitmap *s_res_actionicon_next_white;
 static TextLayer *textlayer_bus_no;
 static TextLayer *textlayer_busstop_name;
 static TextLayer *textlayer_busstop_code;
@@ -20,6 +19,7 @@ static TextLayer *textlayer_nextbus_label;
 static BitmapLayer *bitmaplayer_current;
 static BitmapLayer *bitmaplayer_next;
 static TextLayer *textlayer_debug;
+static ActionBarLayer *actionbar_layer;
 
 static void initialise_ui(void) {
   s_window = window_create();
@@ -27,22 +27,13 @@ static void initialise_ui(void) {
     window_set_fullscreen(s_window, false);
   #endif
   
-  s_res_actionicon_previous_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_PREVIOUS_WHITE);
-  s_res_actionicon_next_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_NEXT_WHITE);
   s_res_gothic_24_bold = fonts_get_system_font(FONT_KEY_GOTHIC_24_BOLD);
   s_res_gothic_14 = fonts_get_system_font(FONT_KEY_GOTHIC_14);
   s_res_gothic_28_bold = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
   s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
   s_res_actionicon_refresh_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_REFRESH_WHITE);
-  // actionbar_layer
-  actionbar_layer = action_bar_layer_create();
-  action_bar_layer_add_to_window(actionbar_layer, s_window);
-  action_bar_layer_set_background_color(actionbar_layer, GColorBlack);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_actionicon_previous_white);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_SELECT, s_res_actionicon_refresh_white);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_actionicon_next_white);
-  layer_add_child(window_get_root_layer(s_window), (Layer *)actionbar_layer);
-  
+  s_res_actionicon_previous_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_PREVIOUS_WHITE);
+  s_res_actionicon_next_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_NEXT_WHITE);
   // textlayer_bus_no
   textlayer_bus_no = text_layer_create(GRect(3, 1, 51, 26));
   text_layer_set_background_color(textlayer_bus_no, GColorClear);
@@ -99,11 +90,19 @@ static void initialise_ui(void) {
   text_layer_set_text(textlayer_debug, "Debug Button");
   text_layer_set_font(textlayer_debug, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)textlayer_debug);
+  
+  // actionbar_layer
+  actionbar_layer = action_bar_layer_create();
+  action_bar_layer_add_to_window(actionbar_layer, s_window);
+  action_bar_layer_set_background_color(actionbar_layer, GColorBlack);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_actionicon_previous_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_SELECT, s_res_actionicon_refresh_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_actionicon_next_white);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)actionbar_layer);
 }
 
 static void destroy_ui(void) {
   window_destroy(s_window);
-  action_bar_layer_destroy(actionbar_layer);
   text_layer_destroy(textlayer_bus_no);
   text_layer_destroy(textlayer_busstop_name);
   text_layer_destroy(textlayer_busstop_code);
@@ -113,9 +112,10 @@ static void destroy_ui(void) {
   bitmap_layer_destroy(bitmaplayer_current);
   bitmap_layer_destroy(bitmaplayer_next);
   text_layer_destroy(textlayer_debug);
+  action_bar_layer_destroy(actionbar_layer);
+  gbitmap_destroy(s_res_actionicon_refresh_white);
   gbitmap_destroy(s_res_actionicon_previous_white);
   gbitmap_destroy(s_res_actionicon_next_white);
-  gbitmap_destroy(s_res_actionicon_refresh_white);
 }
 // END AUTO-GENERATED UI CODE
 
