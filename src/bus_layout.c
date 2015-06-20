b594e09188e73549f6fa73661fabe9feff81fea0
Kenneth Soh <kennethsohyq@hotmail.com>
2015-06-20 17:55:35 +0800

    Added test and moved debug text around
    Don't worry, the debug text isnt permanent lol

--- src/bus_layout.c
+++ src/bus_layout.c
@@ -7,9 +7,10 @@ static GFont s_res_gothic_24_bold;
 static GFont s_res_gothic_14;
 static GFont s_res_gothic_28_bold;
 static GFont s_res_gothic_18_bold;
+static GBitmap *s_res_bus_nodata;
+static GBitmap *s_res_actionicon_next_white;
 static GBitmap *s_res_actionicon_refresh_white;
 static GBitmap *s_res_actionicon_previous_white;
-static GBitmap *s_res_actionicon_next_white;
 static TextLayer *textlayer_bus_no;
 static TextLayer *textlayer_busstop_name;
 static TextLayer *textlayer_busstop_code;
@@ -20,6 +21,7 @@ static BitmapLayer *bitmaplayer_current;
 static BitmapLayer *bitmaplayer_next;
 static TextLayer *textlayer_debug;
 static ActionBarLayer *actionbar_layer;
+static TextLayer *textlayer_pages;
 
 static void initialise_ui(void) {
   s_window = window_create();
@@ -31,9 +33,10 @@ static void initialise_ui(void) {
   s_res_gothic_14 = fonts_get_system_font(FONT_KEY_GOTHIC_14);
   s_res_gothic_28_bold = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
   s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
+  s_res_bus_nodata = gbitmap_create_with_resource(RESOURCE_ID_BUS_NODATA);
+  s_res_actionicon_next_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_NEXT_WHITE);
   s_res_actionicon_refresh_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_REFRESH_WHITE);
   s_res_actionicon_previous_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_PREVIOUS_WHITE);
-  s_res_actionicon_next_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_NEXT_WHITE);
   // textlayer_bus_no
   textlayer_bus_no = text_layer_create(GRect(3, 1, 51, 26));
   text_layer_set_background_color(textlayer_bus_no, GColorClear);
@@ -78,15 +81,18 @@ static void initialise_ui(void) {
   layer_add_child(window_get_root_layer(s_window), (Layer *)textlayer_nextbus_label);
   
   // bitmaplayer_current
-  bitmaplayer_current = bitmap_layer_create(GRect(23, 72, 75, 21));
+  bitmaplayer_current = bitmap_layer_create(GRect(42, 77, 35, 10));
+  bitmap_layer_set_bitmap(bitmaplayer_current, s_res_bus_nodata);
   layer_add_child(window_get_root_layer(s_window), (Layer *)bitmaplayer_current);
   
   // bitmaplayer_next
-  bitmaplayer_next = bitmap_layer_create(GRect(30, 122, 65, 18));
+  bitmaplayer_next = bitmap_layer_create(GRect(42, 123, 35, 10));
+  bitmap_layer_set_bitmap(bitmaplayer_next, s_res_bus_nodata);
   layer_add_child(window_get_root_layer(s_window), (Layer *)bitmaplayer_next);
   
   // textlayer_debug
-  textlayer_debug = text_layer_create(GRect(3, 136, 117, 20));
+  textlayer_debug = text_layer_create(GRect(4, 38, 114, 20));
+  text_layer_set_background_color(textlayer_debug, GColorClear);
   text_layer_set_text(textlayer_debug, "Debug Button");
   text_layer_set_font(textlayer_debug, s_res_gothic_14);
   layer_add_child(window_get_root_layer(s_window), (Layer *)textlayer_debug);
@@ -95,10 +101,18 @@ static void initialise_ui(void) {
   actionbar_layer = action_bar_layer_create();
   action_bar_layer_add_to_window(actionbar_layer, s_window);
   action_bar_layer_set_background_color(actionbar_layer, GColorBlack);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_actionicon_previous_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_actionicon_next_white);
   action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_SELECT, s_res_actionicon_refresh_white);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_actionicon_next_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_actionicon_previous_white);
   layer_add_child(window_get_root_layer(s_window), (Layer *)actionbar_layer);
+  
+  // textlayer_pages
+  textlayer_pages = text_layer_create(GRect(3, 132, 116, 20));
+  text_layer_set_background_color(textlayer_pages, GColorClear);
+  text_layer_set_text(textlayer_pages, "1/100");
+  text_layer_set_text_alignment(textlayer_pages, GTextAlignmentRight);
+  text_layer_set_font(textlayer_pages, s_res_gothic_14);
+  layer_add_child(window_get_root_layer(s_window), (Layer *)textlayer_pages);
 }
 
 static void destroy_ui(void) {
@@ -113,11 +127,17 @@ static void destroy_ui(void) {
   bitmap_layer_destroy(bitmaplayer_next);
   text_layer_destroy(textlayer_debug);
   action_bar_layer_destroy(actionbar_layer);
+  text_layer_destroy(textlayer_pages);
+  gbitmap_destroy(s_res_bus_nodata);
+  gbitmap_destroy(s_res_actionicon_next_white);
   gbitmap_destroy(s_res_actionicon_refresh_white);
   gbitmap_destroy(s_res_actionicon_previous_white);
-  gbitmap_destroy(s_res_actionicon_next_white);
 }
 // END AUTO-GENERATED UI CODE
+
+static GBitmap *bus_available;
+static GBitmap *bus_limited;
+static GBitmap *bus_full;
 
 enum {
     KEY_BUTTON_EVENT = 0,
@@ -147,11 +167,51 @@ void send_int(uint8_t key, uint8_t cmd)
 
 //User Actions
 
+//DEBUG Test Update of pictures
+static void updateMode(char movement){
+  if (movement == 'u'){
+    //Advance
+    if (bitmap_layer_get_bitmap(bitmaplayer_current) == bus_available){
+      //Become bus limited
+      bitmap_layer_set_bitmap(bitmaplayer_next, bus_limited);
+      bitmap_layer_set_bitmap(bitmaplayer_current, bus_limited);
+    } else if (bitmap_layer_get_bitmap(bitmaplayer_current) == bus_limited){
+      //Become bus full
+      bitmap_layer_set_bitmap(bitmaplayer_next, bus_full);
+      bitmap_layer_set_bitmap(bitmaplayer_current, bus_full);
+    } else if (bitmap_layer_get_bitmap(bitmaplayer_current) == s_res_bus_nodata){
+      //Become bus available
+      bitmap_layer_set_bitmap(bitmaplayer_next, bus_available);
+      bitmap_layer_set_bitmap(bitmaplayer_current, bus_available);
+    }
+  } else if (movement == 'd'){
+    //Descend
+    if (bitmap_layer_get_bitmap(bitmaplayer_current) == bus_available){
+      //Become no data
+      bitmap_layer_set_bitmap(bitmaplayer_next, s_res_bus_nodata);
+      bitmap_layer_set_bitmap(bitmaplayer_current, s_res_bus_nodata);
+    } else if (bitmap_layer_get_bitmap(bitmaplayer_current) == bus_limited){
+      //Become bus available
+      bitmap_layer_set_bitmap(bitmaplayer_next, bus_available);
+      bitmap_layer_set_bitmap(bitmaplayer_current, bus_available);
+    } else if (bitmap_layer_get_bitmap(bitmaplayer_current) == bus_full){
+      //Become bus limited
+      bitmap_layer_set_bitmap(bitmaplayer_next, bus_limited);
+      bitmap_layer_set_bitmap(bitmaplayer_current, bus_limited);
+    }
+  } else if (movement == 'c'){
+    //Reset to no data
+    bitmap_layer_set_bitmap(bitmaplayer_next, s_res_bus_nodata);
+    bitmap_layer_set_bitmap(bitmaplayer_current, s_res_bus_nodata);
+  }
+}
+
 // When the select button is clicked
 static void select_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(textlayer_debug, "Select (Refreshes)");
   //TODO Tell android to refresh app
   send_int(KEY_BUTTON_EVENT, BUTTON_REFRESH);
+  updateMode('c');
 }
 
 // When the up button is clicked
@@ -159,6 +219,7 @@ static void up_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(textlayer_debug, "Up (Go Previous)");
   //TODO Go to previous if available
   send_int(KEY_BUTTON_EVENT, BUTTON_PREVIOUS);
+  updateMode('u');
 }
 
 // When the down button is clicked
@@ -166,6 +227,7 @@ static void down_click_handler(ClickRecognizerRef recognizer, void *context) {
   text_layer_set_text(textlayer_debug, "Down (Go Next)");
   //TODO Go to next if available
   send_int(KEY_BUTTON_EVENT, BUTTON_NEXT);
+  updateMode('d');
 }
 
 // The Button Config
@@ -175,12 +237,20 @@ static void click_config_provider(void *context) {
   window_single_click_subscribe(BUTTON_ID_DOWN, down_click_handler);
 }
 
+static void init_bus_indicators(void){
+  bus_available = gbitmap_create_with_resource(RESOURCE_ID_BUS_SEATSAVAIL);
+  bus_limited = gbitmap_create_with_resource(RESOURCE_ID_BUS_SEATSLIMITED);
+  bus_full = gbitmap_create_with_resource(RESOURCE_ID_BUS_SEATSNONE);
+}
+
 static void handle_window_unload(Window* window) {
   destroy_ui();
 }
 
 void show_bus_layout(void) {
   initialise_ui();
+  init_bus_indicators();
+  
   action_bar_layer_set_click_config_provider(actionbar_layer, click_config_provider);
   window_set_window_handlers(s_window, (WindowHandlers) {
     .unload = handle_window_unload,
