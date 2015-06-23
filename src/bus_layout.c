46a8efa63eedcb4ab90576b56357780f8b376741
Kenneth Soh <kennethsohyq@hotmail.com>
2015-06-24 00:08:15 +0800

    Final Bug Fixes
    - Fixed Action Bar Previous and Next Icons in the wrong position
    - Fixed Paging text layer not displaying anything (Herp Derp, after crafting the buffer I forgot to set it into the textlayer lol. :facepalm:)

--- src/bus_layout.c
+++ src/bus_layout.c
@@ -8,9 +8,9 @@ static GFont s_res_gothic_14;
 static GFont s_res_gothic_28_bold;
 static GFont s_res_gothic_18_bold;
 static GBitmap *s_res_bus_nodata;
-static GBitmap *s_res_actionicon_next_white;
 static GBitmap *s_res_actionicon_refresh_white;
 static GBitmap *s_res_actionicon_previous_white;
+static GBitmap *s_res_actionicon_next_white;
 static TextLayer *textlayer_bus_no;
 static TextLayer *textlayer_busstop_name;
 static TextLayer *textlayer_busstop_code;
@@ -34,9 +34,9 @@ static void initialise_ui(void) {
   s_res_gothic_28_bold = fonts_get_system_font(FONT_KEY_GOTHIC_28_BOLD);
   s_res_gothic_18_bold = fonts_get_system_font(FONT_KEY_GOTHIC_18_BOLD);
   s_res_bus_nodata = gbitmap_create_with_resource(RESOURCE_ID_BUS_NODATA);
-  s_res_actionicon_next_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_NEXT_WHITE);
   s_res_actionicon_refresh_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_REFRESH_WHITE);
   s_res_actionicon_previous_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_PREVIOUS_WHITE);
+  s_res_actionicon_next_white = gbitmap_create_with_resource(RESOURCE_ID_ACTIONICON_NEXT_WHITE);
   // textlayer_bus_no
   textlayer_bus_no = text_layer_create(GRect(3, 1, 51, 26));
   text_layer_set_background_color(textlayer_bus_no, GColorClear);
@@ -101,9 +101,9 @@ static void initialise_ui(void) {
   actionbar_layer = action_bar_layer_create();
   action_bar_layer_add_to_window(actionbar_layer, s_window);
   action_bar_layer_set_background_color(actionbar_layer, GColorBlack);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_actionicon_next_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_UP, s_res_actionicon_previous_white);
   action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_SELECT, s_res_actionicon_refresh_white);
-  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_actionicon_previous_white);
+  action_bar_layer_set_icon(actionbar_layer, BUTTON_ID_DOWN, s_res_actionicon_next_white);
   layer_add_child(window_get_root_layer(s_window), (Layer *)actionbar_layer);
   
   // textlayer_pages
@@ -129,9 +129,9 @@ static void destroy_ui(void) {
   action_bar_layer_destroy(actionbar_layer);
   text_layer_destroy(textlayer_pages);
   gbitmap_destroy(s_res_bus_nodata);
-  gbitmap_destroy(s_res_actionicon_next_white);
   gbitmap_destroy(s_res_actionicon_refresh_white);
   gbitmap_destroy(s_res_actionicon_previous_white);
+  gbitmap_destroy(s_res_actionicon_next_white);
 }
 // END AUTO-GENERATED UI CODE
 
@@ -281,6 +281,7 @@ static void in_received_handler(DictionaryIterator *iter, void *context){
     static char paging_buffer[10];
     if (pgLoad && pgMaxLoad){
       snprintf(paging_buffer, sizeof(paging_buffer), "%d/%d", current, max);
+      text_layer_set_text(textlayer_pages, paging_buffer);
     }
 
     // Get next pair, if any
