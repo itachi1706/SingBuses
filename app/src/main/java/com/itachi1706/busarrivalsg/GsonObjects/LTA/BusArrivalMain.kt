95156706a0f34d8ba837e5f9a48384cf543b98a0
itachi1706 <kennethsohyq@gmail.com>
2018-08-28 00:15:58 +0800

    Convert LTA GSON Objects to Kotlin

---
+++ app/src/main/java/com/itachi1706/busarrivalsg/GsonObjects/LTA/BusArrivalMain.kt
@@ -0,0 +1,12 @@
+package com.itachi1706.busarrivalsg.GsonObjects.LTA
+
+/**
+ * Created by Kenneth on 18/6/2015
+ * for SingBuses in package com.itachi1706.busarrivalsg.GsonObjects.LTA
+ */
+class BusArrivalMain {
+
+    val busStopCode: String? = null
+    val services: Array<BusArrivalArrayObject>? = null
+    val currentTime: String? = null
+}
---
+++ app/src/main/java/com/itachi1706/busarrivalsg/GsonObjects/LTA/BusStopJSONArray.kt
@@ -0,0 +1,10 @@
+package com.itachi1706.busarrivalsg.GsonObjects.LTA
+
+/**
+ * Created by Kenneth on 18/6/2015
+ * for SingBuses in package com.itachi1706.busarrivalsg.GsonObjects.LTA
+ */
+class BusStopJSONArray {
+
+    val busStopsArray: Array<BusStopJSON>? = null
+}
