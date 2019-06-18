a44dbe77ba9f110079580ae40b08610db46b49c1
itachi1706 <kennethsohyq@gmail.com>
2019-06-18 21:24:27 +0800

    Updated to the new CEPAS library and added OSS license

--- app/src/main/java/com/itachi1706/busarrivalsg/CEPASScanActivity.java
+++ app/src/main/java/com/itachi1706/busarrivalsg/CEPASScanActivity.java
@@ -1,20 +1,29 @@
 package com.itachi1706.busarrivalsg;
 
+import android.content.Intent;
 import android.os.Bundle;
 
-import com.itachi1706.cepaslib.fragment.CEPASCardScanFragment;
-
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
+
+import com.itachi1706.appupdater.Util.PrefHelper;
+import com.itachi1706.cepaslib.CEPASLibBuilder;
+import com.itachi1706.cepaslib.app.feature.main.MainActivity;
 
 public class CEPASScanActivity extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        if (getSupportFragmentManager().findFragmentById(android.R.id.content)==null) {
-            getSupportFragmentManager().beginTransaction()
-                    .add(android.R.id.content, new CEPASCardScanFragment())
-                    .commit();
-        }
+        PrefHelper.changeDarkModeTheme(AppCompatDelegate.MODE_NIGHT_YES, "default dark"); // TODO: Default app is on dark theme, remove after we support day/night mode
+        CEPASLibBuilder builder = CEPASLibBuilder.INSTANCE;
+        builder.setPreferenceClass(MainSettings.class);
+        builder.updateTitleBarColor(R.color.primary);
+        builder.updateAccentColor(R.color.accent);
+        builder.updateErrorColor(R.color.primaryDark);
+        builder.setCustomTitle("Scan EZ-Link Card");
+        builder.setHomeScreenWithBackButton(true);
+        startActivity(new Intent(this, MainActivity.class));
+        finish();
     }
 }
