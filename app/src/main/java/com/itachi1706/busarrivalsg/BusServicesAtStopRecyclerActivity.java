03bc0c136e22cc39c600b5ddd2eff95da10761f5
itachi1706 <kennethsohyq@gmail.com>
2020-06-24 20:19:38 +0800

    [SGBUSAND-209] Implemented Theoratical fix for resource not being found on some devices
    This is from one of the stackoverflow suggestions that we add this to places calling the bitmap just in case it did not work
    
    Closes SGBUSAND-209
    
    Took 17 minutes

--- app/src/main/java/com/itachi1706/busarrivalsg/BusServicesAtStopRecyclerActivity.java
+++ app/src/main/java/com/itachi1706/busarrivalsg/BusServicesAtStopRecyclerActivity.java
@@ -14,6 +14,7 @@ import android.view.MenuItem;
 import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.appcompat.app.AppCompatDelegate;
 import androidx.collection.ArrayMap;
 import androidx.recyclerview.widget.DefaultItemAnimator;
 import androidx.recyclerview.widget.ItemTouchHelper;
@@ -38,6 +39,8 @@ import java.util.ArrayList;
 import java.util.Map;
 
 public class BusServicesAtStopRecyclerActivity extends AppCompatActivity implements SwipeRefreshLayout.OnRefreshListener, IHandleStuff {
+
+    static { AppCompatDelegate.setCompatVectorFromResourcesEnabled(true); }
 
     RecyclerView buses;
     String busStopCode, busStopName, busServicesString;
