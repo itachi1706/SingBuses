e6dcccd1b06fd7947f9d14e0227480336a6885a7
Kenneth Soh <kenneth@itachi1706.com>
2024-06-26 17:21:15 +0800

    refactor: Handle null checks and naming conventions

--- app/src/main/java/com/itachi1706/busarrivalsg/gsonObjects/sgLTA/BusArrivalMain.kt
+++ app/src/main/java/com/itachi1706/busarrivalsg/gsonObjects/sgLTA/BusArrivalMain.kt
@@ -2,31 +2,37 @@
 
 package com.itachi1706.busarrivalsg.gsonObjects.sgLTA
 
+import com.google.gson.annotations.SerializedName
+
 /**
  * Created by Kenneth on 18/6/2015
  * for SingBuses in package com.itachi1706.busarrivalsg.gsonObjects.sgLTA
  */
-data class BusArrivalMain(val BusStopCode: String? = null, val Services: Array<BusArrivalArrayObject>? = null, val CurrentTime: String? = null) {
+data class BusArrivalMain(
+    @SerializedName("BusStopCode") val busStopCode: String? = null,
+    @SerializedName("Services") val services: Array<BusArrivalArrayObject>? = null,
+    @SerializedName("CurrentTime") val currentTime: String? = null
+) {
     override fun equals(other: Any?): Boolean {
         if (this === other) return true
         if (javaClass != other?.javaClass) return false
 
         other as BusArrivalMain
 
-        if (BusStopCode != other.BusStopCode) return false
-        if (Services != null) {
-            if (other.Services == null) return false
-            if (!Services.contentEquals(other.Services)) return false
-        } else if (other.Services != null) return false
-        if (CurrentTime != other.CurrentTime) return false
+        if (busStopCode != other.busStopCode) return false
+        if (services != null) {
+            if (other.services == null) return false
+            if (!services.contentEquals(other.services)) return false
+        } else if (other.services != null) return false
+        if (currentTime != other.currentTime) return false
 
         return true
     }
 
     override fun hashCode(): Int {
-        var result = BusStopCode?.hashCode() ?: 0
-        result = 31 * result + (Services?.contentHashCode() ?: 0)
-        result = 31 * result + (CurrentTime?.hashCode() ?: 0)
+        var result = busStopCode?.hashCode() ?: 0
+        result = 31 * result + (services?.contentHashCode() ?: 0)
+        result = 31 * result + (currentTime?.hashCode() ?: 0)
         return result
     }
 }
