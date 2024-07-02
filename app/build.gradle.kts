efbb5ee116e8afbf500ccd87c5f2d2271d079799
Kenneth Soh <kenneth@itachi1706.com>
2024-07-02 17:28:21 +0800

    feat: Bump Min SDK up to 21

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -17,7 +17,7 @@ android {
     defaultConfig {
         namespace = appNamespace
         applicationId = appNamespace
-        minSdk = 19
+        minSdk = 21
         targetSdk = 34
         versionCode = 937
         versionName = "5.2.2"
