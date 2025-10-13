d66ebf90e9eccde2ea5cea0b843aa46e7878f3d3
Kenneth Soh <kenneth@itachi1706.com>
2025-10-13 19:45:55 +0800

    chore: Remove unused import

--- app/src/main/java/com/itachi1706/busarrivalsg/BusStopsTabbedActivity.kt
+++ app/src/main/java/com/itachi1706/busarrivalsg/BusStopsTabbedActivity.kt
@@ -1,7 +1,6 @@
 package com.itachi1706.busarrivalsg
 
 import android.Manifest
-import android.app.AlertDialog
 import android.content.Intent
 import android.content.pm.PackageManager
 import android.location.Location
